92a66f98ae971c4adaeb0291680cd17a63149af2
Fan Kiat Chan <fchan5@illinois.edu>
2022-09-21 20:36:42 -0500

    feat: inplane field curl 2d

--- sopht_mpi/numeric/eulerian_grid_ops/stencil_ops_2d/__init__.py
+++ sopht_mpi/numeric/eulerian_grid_ops/stencil_ops_2d/__init__.py
@@ -8,3 +8,4 @@ from .advection_flux_mpi_2d import (
 from .advection_timestep_mpi_2d import (
     gen_advection_timestep_euler_forward_conservative_eno3_pyst_mpi_kernel_2d,
 )
+from .inplane_field_curl_mpi_2d import gen_inplane_field_curl_pyst_mpi_kernel_2d
