d625b609b457bf3735fb87f544aa5b59b7362bcc
Fan Kiat Chan <fchan5@illinois.edu>
2022-09-29 16:58:32 -0500

    feat : test case for unbounded poisson solve

---
+++ tests/test_numeric/test_eulerian_grid_ops/test_poisson_solver_2d/test_unbounded_poisson_solver_mpi_2d.py
@@ -0,0 +1,97 @@
+import numpy as np
+import pytest
+from sopht.utils.precision import get_real_t, get_test_tol
+from sopht.numeric.eulerian_grid_ops.poisson_solver_2d import (
+    UnboundedPoissonSolverPYFFTW2D,
+)
+from sopht_mpi.utils import (
+    MPIConstruct2D,
+    MPIFieldCommunicator2D,
+)
+from sopht_mpi.numeric.eulerian_grid_ops.poisson_solver_2d import (
+    UnboundedPoissonSolverMPI2D,
+)
+import os
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
+
+
+@pytest.mark.mpi(group="MPI_unbounded_poisson_solve_2d", min_size=2)
+@pytest.mark.parametrize("ghost_size", [1, 2, 3])
+@pytest.mark.parametrize("precision", ["single", "double"])
+@pytest.mark.parametrize("rank_distribution", [(1, 0), (0, 1)])
+@pytest.mark.parametrize("aspect_ratio", [(1, 1), (1, 2), (2, 1)])
+def test_mpi_unbounded_poisson_solve_2d(
+    ghost_size, precision, rank_distribution, aspect_ratio
+):
+    n_values = 32
+    real_t = get_real_t(precision)
+    # Generate the MPI topology minimal object
+    mpi_construct = MPIConstruct2D(
+        grid_size_y=n_values * aspect_ratio[0],
+        grid_size_x=n_values * aspect_ratio[1],
+        real_t=real_t,
+        rank_distribution=rank_distribution,
+    )
+
+    # Create unbounded poisson solver
+    unbounded_poisson_solver = UnboundedPoissonSolverMPI2D(
+        grid_size_y=n_values * aspect_ratio[0],
+        grid_size_x=n_values * aspect_ratio[1],
+        real_t=real_t,
+        mpi_construct=mpi_construct,
+        ghost_size=ghost_size,
+    )
+
+    # Initialize communicator for scatter and gather
+    mpi_field_comm = MPIFieldCommunicator2D(
+        ghost_size=ghost_size, mpi_construct=mpi_construct
+    )
+    gather_local_field = mpi_field_comm.gather_local_field
+    scatter_global_field = mpi_field_comm.scatter_global_field
+
+    # Allocate local field
+    local_rhs_field = np.zeros(
+        (
+            mpi_construct.local_grid_size[0] + 2 * ghost_size,
+            mpi_construct.local_grid_size[1] + 2 * ghost_size,
+        )
+    ).astype(real_t)
+    local_solution_field = np.zeros_like(local_rhs_field)
+
+    # Initialize and broadcast solution for comparison later
+    if mpi_construct.rank == 0:
+        ref_rhs_field = np.random.rand(
+            n_values * aspect_ratio[0], n_values * aspect_ratio[1]
+        ).astype(real_t)
+    else:
+        ref_rhs_field = None
+
+    # scatter global field
+    scatter_global_field(local_rhs_field, ref_rhs_field, mpi_construct)
+
+    # compute the unbounded poisson solve
+    unbounded_poisson_solver.solve(
+        solution_field=local_solution_field, rhs_field=local_rhs_field
+    )
+
+    # gather back the solution field globally
+    global_solution_field = np.zeros_like(ref_rhs_field)
+    gather_local_field(global_solution_field, local_solution_field, mpi_construct)
+
+    # assert correct
+    if mpi_construct.rank == 0:
+        ref_unbounded_poisson_solver = UnboundedPoissonSolverPYFFTW2D(
+            grid_size_y=n_values * aspect_ratio[0],
+            grid_size_x=n_values * aspect_ratio[1],
+            real_t=real_t,
+        )
+        ref_solution_field = np.zeros_like(ref_rhs_field)
+        ref_unbounded_poisson_solver.solve(
+            solution_field=ref_solution_field, rhs_field=ref_rhs_field
+        )
+        np.testing.assert_allclose(
+            ref_solution_field,
+            global_solution_field,
+            atol=get_test_tol(precision),
+        )
