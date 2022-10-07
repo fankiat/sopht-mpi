39af74d2fa5f220fc21475133ee640daf4601a88
Fan Kiat Chan <fchan5@illinois.edu>
2022-10-07 16:08:33 -0500

    update: get caller function

--- sopht_mpi/utils/mpi_utils.py
+++ sopht_mpi/utils/mpi_utils.py
@@ -2,8 +2,16 @@
 import inspect
 
 
-def _get_caller_name():
-    return inspect.currentframe().f_back.f_back.f_code.co_name
+def _get_caller_name(steps=1):
+    """
+    Get calling function's name
+    """
+    frame = inspect.currentframe()
+    # step back once here to account for _get_caller_name function itself
+    frame = frame.f_back
+    for i in range(steps):
+        frame = frame.f_back
+    return frame.f_code.co_name
 
 
 def check_valid_ghost_size_and_kernel_support(ghost_size, kernel_support):
@@ -11,6 +19,6 @@ def check_valid_ghost_size_and_kernel_support(ghost_size, kernel_support):
     if ghost_size < kernel_support:
         raise ValueError(
             f"Inconsistent ghost_size={ghost_size} and kernel_support="
-            f"{kernel_support} for kernel {_get_caller_name()}. "
+            f"{kernel_support} for kernel {_get_caller_name(steps=1)}. "
             "Need to have ghost_size >= kernel_support"
         )
