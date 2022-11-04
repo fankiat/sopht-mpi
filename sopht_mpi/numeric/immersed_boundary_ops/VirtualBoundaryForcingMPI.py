d25338903bc73ec1aac787c3ff4dec011ac79bce
Fan Kiat <fankiat@hotmail.com>
2022-11-03 20:34:56 -0500

    feat : add global lag grid position as initialization argument for virtual boundary forcing class

--- sopht_mpi/numeric/immersed_boundary_ops/VirtualBoundaryForcingMPI.py
+++ sopht_mpi/numeric/immersed_boundary_ops/VirtualBoundaryForcingMPI.py
@@ -15,6 +15,8 @@ from sopht_mpi.numeric.immersed_boundary_ops.EulerianLagrangianGridCommunicatorM
 # from sopht.numeric.immersed_boundary_ops.EulerianLagrangianGridCommunicator3D import (
 #     EulerianLagrangianGridCommunicator3D,
 # )
+from sopht_mpi.utils.mpi_utils_2d import MPILagrangianFieldCommunicator2D
+from mpi4py import MPI
 
 
 class VirtualBoundaryForcingMPI:
@@ -24,6 +26,9 @@ class VirtualBoundaryForcingMPI:
     Virtual boundary forcing class for computing feedback between the
     Lagrangian body and Eulerian grid flow, using virtual boundary method
     Refer to Goldstein 1993, JCP for details on the penalty force computation.
+
+    Note: For clarify and explicit naming, buffer variables here are prefixed with
+    'local_*' and 'global_*' to indicate mpi local and global variables, respectively.
     """
 
     def __init__(
@@ -34,12 +39,14 @@ class VirtualBoundaryForcingMPI:
         virtual_boundary_damping_coeff,
         grid_dim,
         dx,
-        num_lag_nodes,
         real_t,
         eul_grid_coord_shift=None,
         interp_kernel_width=None,
         enable_eul_grid_forcing_reset=True,
         start_time=0.0,
+        master_rank=0,
+        global_lag_grid_position_field=None,
+        moving_boundary=True,
     ):
         """Class initialiser.
 
@@ -52,27 +59,25 @@ class VirtualBoundaryForcingMPI:
         dx: Eulerian grid spacing
         eul_grid_coord_shift: shift of the coordinates of the Eulerian grid start from
         0 (usually dx / 2)
-        num_lag_nodes: number of Lagrangian grid nodes
+        global_num_lag_nodes: total number of Lagrangian grid nodes
         interp_kernel_width: width of interpolation kernel
         real_t: numerical precision
         enable_eul_grid_forcing_reset : flag for enabling option of feedback step
         with resetting of eul_grid_forcing_field
-        num_threads: number of threads (only for the resetting function)
         start_time: start time of the forcing
+        master_rank: rank that owns all the lagrangian nodes and serves as main comm hub
+        global_lag_grid_position_field: initial lagrangian grid position
+        moving_boundary: flag for moving boundary, used in buffer reinitialization
 
         """
         if grid_dim != 2 and grid_dim != 3:
             raise ValueError("Invalid grid dimensions for virtual boundary forcing!")
         self.grid_dim = grid_dim
-        self.local_num_lag_nodes = num_lag_nodes
-        self.supported_num_lag_nodes_in_buffers = num_lag_nodes
         self.virtual_boundary_stiffness_coeff = virtual_boundary_stiffness_coeff
         self.virtual_boundary_damping_coeff = virtual_boundary_damping_coeff
         self.time = start_time
         self.real_t = real_t
-
-        self.mpi_construct = mpi_construct
-        self.ghost_size = ghost_size
+        self.moving_boundary = moving_boundary
 
         # these are rather invariant hence pushed to fixed kwargs
         if eul_grid_coord_shift is None:
@@ -80,14 +85,36 @@ class VirtualBoundaryForcingMPI:
         self.interp_kernel_width = interp_kernel_width
         if interp_kernel_width is None:
             self.interp_kernel_width = 2
+        self.ghost_size = ghost_size
 
-        if self.interp_kernel_width > self.ghost_size:
+        if self.interp_kernel_width > ghost_size:
             raise ValueError(
-                f"Field ghost size {self.ghost_size} needs to be larger than interpolation kernel width {self.interp_kernel_width}"
+                f"Field ghost size {ghost_size} needs to be larger than "
+                f"interpolation kernel width {self.interp_kernel_width}"
             )
 
+        # Initialize MPI related stuff
+        self.mpi_construct = mpi_construct
+        self.mpi_lagrangian_field_communicator = MPILagrangianFieldCommunicator2D(
+            eul_grid_dx=dx,
+            eul_grid_coord_shift=eul_grid_coord_shift,
+            mpi_construct=self.mpi_construct,
+            master_rank=master_rank,
+            real_t=real_t,
+        )
+        self.mpi_lagrangian_field_communicator.map_lagrangian_nodes_based_on_position(
+            global_lag_positions=global_lag_grid_position_field
+        )
+        self.local_num_lag_nodes = (
+            self.mpi_lagrangian_field_communicator.local_num_lag_nodes
+        )
+        self.global_num_lag_nodes = (
+            self.mpi_lagrangian_field_communicator.rank_address.shape[-1]
+        )
+
         # creating buffers...
-        self._init_buffers(self.supported_num_lag_nodes_in_buffers)
+        self._init_local_buffers(self.local_num_lag_nodes)
+        self._init_global_buffers()
 
         if grid_dim == 2:
             self.eul_lag_grid_communicator = EulerianLagrangianGridCommunicatorMPI2D(
@@ -130,8 +157,16 @@ class VirtualBoundaryForcingMPI:
                 self.compute_interaction_force_on_eul_and_lag_grid
             )
 
-    def _init_buffers(self, num_lag_nodes):
-        self.nearest_eul_grid_index_to_lag_grid = np.empty(
+    def _init_global_buffers(self):
+        self.global_lag_grid_position_mismatch_field = np.zeros(
+            (self.grid_dim, self.global_num_lag_nodes), dtype=self.real_t
+        )
+        self.global_lag_grid_velocity_mismatch_field = np.zeros_like(
+            self.global_lag_grid_position_mismatch_field
+        )
+
+    def _init_local_buffers(self, num_lag_nodes):
+        self.local_nearest_eul_grid_index_to_lag_grid = np.empty(
             (self.grid_dim, num_lag_nodes), dtype=int
         )
         eul_grid_support_of_lag_grid_shape = (
@@ -139,28 +174,75 @@ class VirtualBoundaryForcingMPI:
             + (2 * self.interp_kernel_width,) * self.grid_dim
             + (num_lag_nodes,)
         )
-        self.local_eul_grid_support_of_lag_grid = np.empty(
+        self.local_local_eul_grid_support_of_lag_grid = np.empty(
             eul_grid_support_of_lag_grid_shape, dtype=self.real_t
         )
         interp_weights_shape = (2 * self.interp_kernel_width,) * self.grid_dim + (
             num_lag_nodes,
         )
-        self.interp_weights = np.empty(interp_weights_shape, dtype=self.real_t)
-        self.lag_grid_flow_velocity_field = np.zeros(
+        self.local_interp_weights = np.empty(interp_weights_shape, dtype=self.real_t)
+        self.local_lag_grid_flow_velocity_field = np.zeros(
             (self.grid_dim, num_lag_nodes), dtype=self.real_t
         )
-        self.lag_grid_position_mismatch_field = np.zeros_like(
-            self.lag_grid_flow_velocity_field
+        self.local_lag_grid_position_mismatch_field = np.zeros_like(
+            self.local_lag_grid_flow_velocity_field
         )
-        self.lag_grid_velocity_mismatch_field = np.zeros_like(
-            self.lag_grid_position_mismatch_field
+        self.local_lag_grid_velocity_mismatch_field = np.zeros_like(
+            self.local_lag_grid_position_mismatch_field
         )
-        self.lag_grid_forcing_field = np.zeros_like(
-            self.lag_grid_velocity_mismatch_field
+        self.local_lag_grid_forcing_field = np.zeros_like(
+            self.local_lag_grid_velocity_mismatch_field
+        )
+        self.local_lag_grid_position_field = np.zeros_like(
+            self.local_lag_grid_position_mismatch_field
+        )
+        self.local_lag_grid_velocity_field = np.zeros_like(
+            self.local_lag_grid_position_field
         )
 
-    def update_buffers(self):
-        self._init_buffers(self.supported_num_lag_nodes_in_buffers)
+    def update_buffers(self, global_lag_grid_position_field):
+        # First gather the local mismatch fields for storing and redistribution later
+        # This needs to be done before remapping.
+        self.mpi_lagrangian_field_communicator.gather_local_field(
+            global_lag_field=self.global_lag_grid_position_mismatch_field,
+            local_lag_field=self.local_lag_grid_position_mismatch_field[
+                ..., : self.local_num_lag_nodes
+            ],
+        )
+        self.mpi_lagrangian_field_communicator.gather_local_field(
+            global_lag_field=self.global_lag_grid_velocity_mismatch_field,
+            local_lag_field=self.local_lag_grid_velocity_mismatch_field[
+                ..., : self.local_num_lag_nodes
+            ],
+        )
+        # Re-map the lagrangian nodes
+        self.mpi_lagrangian_field_communicator.map_lagrangian_nodes_based_on_position(
+            global_lag_grid_position_field
+        )
+        # Check if local buffers needs to be updated
+        update_buffer_flag = (
+            self.local_num_lag_nodes
+            != self.mpi_lagrangian_field_communicator.local_num_lag_nodes
+        )
+        update_buffer_flag = self.mpi_construct.grid.allreduce(
+            update_buffer_flag, op=MPI.LOR
+        )
+        # Reinitialize buffers if needed
+        if update_buffer_flag:
+            # Initialize buffer with new local size
+            self.local_num_lag_nodes = (
+                self.mpi_lagrangian_field_communicator.local_num_lag_nodes
+            )
+            self._init_local_buffers(self.local_num_lag_nodes)
+            # Scatter the global mismatch fields correspondingly
+            self.mpi_lagrangian_field_communicator.scatter_global_field(
+                local_lag_field=self.local_lag_grid_position_mismatch_field,
+                global_lag_field=self.global_lag_grid_position_mismatch_field,
+            )
+            self.mpi_lagrangian_field_communicator.scatter_global_field(
+                local_lag_field=self.local_lag_grid_velocity_mismatch_field,
+                global_lag_field=self.global_lag_grid_velocity_mismatch_field,
+            )
 
     @staticmethod
     @njit(cache=True, fastmath=True)
@@ -219,70 +301,80 @@ class VirtualBoundaryForcingMPI:
 
     def compute_interaction_force_on_lag_grid(
         self,
-        eul_grid_velocity_field,
-        lag_grid_position_field,
-        lag_grid_velocity_field,
+        local_eul_grid_velocity_field,
+        global_lag_grid_position_field,
+        global_lag_grid_velocity_field,
     ):
         """Virtual boundary: compute interaction force on Lagrangian grid."""
+        # Start by checking and mapping global lag grid fields data
         # Check and make sure buffer is enough to store all local lagrangian quantities
-        # Otherwise, reinitialize buffers twice the size
-        self.local_num_lag_nodes = lag_grid_position_field.shape[-1]
-        if self.local_num_lag_nodes != self.supported_num_lag_nodes_in_buffers:
-            self.supported_num_lag_nodes_in_buffers = self.local_num_lag_nodes
-            self.update_buffers()
+        if self.moving_boundary:
+            self.update_buffers(
+                global_lag_grid_position_field=global_lag_grid_position_field
+            )
+        # Distribute the global lag grid fields
+        self.mpi_lagrangian_field_communicator.scatter_global_field(
+            local_lag_field=self.local_lag_grid_position_field,
+            global_lag_field=global_lag_grid_position_field,
+        )
+        self.mpi_lagrangian_field_communicator.scatter_global_field(
+            local_lag_field=self.local_lag_grid_velocity_field,
+            global_lag_field=global_lag_grid_velocity_field,
+        )
 
+        # Start working on the local chunks of data
         # 1. Find Eulerian grid local support of the Lagrangian grid
         self.eul_lag_grid_communicator.local_eulerian_grid_support_of_lagrangian_grid_kernel(
-            local_eul_grid_support_of_lag_grid=self.local_eul_grid_support_of_lag_grid[
+            local_eul_grid_support_of_lag_grid=self.local_local_eul_grid_support_of_lag_grid[
                 ..., : self.local_num_lag_nodes
             ],
-            nearest_eul_grid_index_to_lag_grid=self.nearest_eul_grid_index_to_lag_grid[
+            nearest_eul_grid_index_to_lag_grid=self.local_nearest_eul_grid_index_to_lag_grid[
                 ..., : self.local_num_lag_nodes
             ],
-            lag_positions=lag_grid_position_field,
+            lag_positions=self.local_lag_grid_position_field,
         )
 
         # 2. Compute interpolation weights based on local Eulerian grid support
         self.eul_lag_grid_communicator.interpolation_weights_kernel(
-            interp_weights=self.interp_weights[..., : self.local_num_lag_nodes],
-            local_eul_grid_support_of_lag_grid=self.local_eul_grid_support_of_lag_grid[
+            interp_weights=self.local_interp_weights[..., : self.local_num_lag_nodes],
+            local_eul_grid_support_of_lag_grid=self.local_local_eul_grid_support_of_lag_grid[
                 ..., : self.local_num_lag_nodes
             ],
         )
 
         # 3. Interpolate Eulerian flow velocity onto the Lagrangian grid
         self.eul_lag_grid_communicator.eulerian_to_lagrangian_grid_interpolation_kernel(
-            lag_grid_field=self.lag_grid_flow_velocity_field[
+            lag_grid_field=self.local_lag_grid_flow_velocity_field[
                 ..., : self.local_num_lag_nodes
             ],
-            eul_grid_field=eul_grid_velocity_field,
-            interp_weights=self.interp_weights[..., : self.local_num_lag_nodes],
-            nearest_eul_grid_index_to_lag_grid=self.nearest_eul_grid_index_to_lag_grid[
+            eul_grid_field=local_eul_grid_velocity_field,
+            interp_weights=self.local_interp_weights[..., : self.local_num_lag_nodes],
+            nearest_eul_grid_index_to_lag_grid=self.local_nearest_eul_grid_index_to_lag_grid[
                 ..., : self.local_num_lag_nodes
             ],
         )
 
         # 4. Compute velocity mismatch between flow and body on Lagrangian grid
         self.compute_lag_grid_velocity_mismatch_field(
-            lag_grid_velocity_mismatch_field=self.lag_grid_velocity_mismatch_field[
+            lag_grid_velocity_mismatch_field=self.local_lag_grid_velocity_mismatch_field[
                 ..., : self.local_num_lag_nodes
             ],
-            lag_grid_flow_velocity_field=self.lag_grid_flow_velocity_field[
+            lag_grid_flow_velocity_field=self.local_lag_grid_flow_velocity_field[
                 ..., : self.local_num_lag_nodes
             ],
-            lag_grid_body_velocity_field=lag_grid_velocity_field,
+            lag_grid_body_velocity_field=self.local_lag_grid_velocity_field,
         )
 
         # 5. Compute penalty force using virtual boundary forcing formulation
         # on Lagrangian grid
         self.compute_lag_grid_forcing_field(
-            lag_grid_forcing_field=self.lag_grid_forcing_field[
+            lag_grid_forcing_field=self.local_lag_grid_forcing_field[
                 ..., : self.local_num_lag_nodes
             ],
-            lag_grid_position_mismatch_field=self.lag_grid_position_mismatch_field[
+            lag_grid_position_mismatch_field=self.local_lag_grid_position_mismatch_field[
                 ..., : self.local_num_lag_nodes
             ],
-            lag_grid_velocity_mismatch_field=self.lag_grid_velocity_mismatch_field[
+            lag_grid_velocity_mismatch_field=self.local_lag_grid_velocity_mismatch_field[
                 ..., : self.local_num_lag_nodes
             ],
             virtual_boundary_stiffness_coeff=self.virtual_boundary_stiffness_coeff,
@@ -291,58 +383,59 @@ class VirtualBoundaryForcingMPI:
 
     def compute_interaction_force_on_eul_and_lag_grid(
         self,
-        eul_grid_forcing_field,
-        eul_grid_velocity_field,
-        lag_grid_position_field,
-        lag_grid_velocity_field,
+        local_eul_grid_forcing_field,
+        local_eul_grid_velocity_field,
+        global_lag_grid_position_field,
+        global_lag_grid_velocity_field,
     ):
         """Virtual boundary: compute interaction on Eulerian grid."""
         # 1. Compute penalty force using virtual boundary forcing formulation
         # on Lagrangian grid
         self.compute_interaction_force_on_lag_grid(
-            eul_grid_velocity_field,
-            lag_grid_position_field,
-            lag_grid_velocity_field,
+            local_eul_grid_velocity_field,
+            global_lag_grid_position_field,
+            global_lag_grid_velocity_field,
         )
         # 2. Interpolate penalty forcing from Lagrangian onto the Eulerian grid
-        self.local_num_lag_nodes = lag_grid_position_field.shape[-1]
         self.eul_lag_grid_communicator.lagrangian_to_eulerian_grid_interpolation_kernel(
-            eul_grid_field=eul_grid_forcing_field,
-            lag_grid_field=self.lag_grid_forcing_field[..., : self.local_num_lag_nodes],
-            interp_weights=self.interp_weights[..., : self.local_num_lag_nodes],
-            nearest_eul_grid_index_to_lag_grid=self.nearest_eul_grid_index_to_lag_grid[
+            eul_grid_field=local_eul_grid_forcing_field,
+            lag_grid_field=self.local_lag_grid_forcing_field[
+                ..., : self.local_num_lag_nodes
+            ],
+            interp_weights=self.local_interp_weights[..., : self.local_num_lag_nodes],
+            nearest_eul_grid_index_to_lag_grid=self.local_nearest_eul_grid_index_to_lag_grid[
                 ..., : self.local_num_lag_nodes
             ],
         )
 
     def compute_interaction_force_on_eul_and_lag_grid_with_eul_grid_forcing_reset(
         self,
-        eul_grid_forcing_field,
-        eul_grid_velocity_field,
-        lag_grid_position_field,
-        lag_grid_velocity_field,
+        local_eul_grid_forcing_field,
+        local_eul_grid_velocity_field,
+        global_lag_grid_position_field,
+        global_lag_grid_velocity_field,
     ):
         """Virtual boundary: compute interaction on Eulerian grid.
 
         Resets eul_grid_forcing_field.
         """
         self.set_eul_grid_vector_field(
-            vector_field=eul_grid_forcing_field, fixed_vals=([0] * self.grid_dim)
+            vector_field=local_eul_grid_forcing_field, fixed_vals=([0] * self.grid_dim)
         )
         self.compute_interaction_force_on_eul_and_lag_grid(
-            eul_grid_forcing_field,
-            eul_grid_velocity_field,
-            lag_grid_position_field,
-            lag_grid_velocity_field,
+            local_eul_grid_forcing_field,
+            local_eul_grid_velocity_field,
+            global_lag_grid_position_field,
+            global_lag_grid_velocity_field,
         )
 
     def time_step(self, dt):
         """Virtual boundary forcing time step, updates grid deviation."""
         self.update_lag_grid_position_mismatch_field_via_euler_forward(
-            lag_grid_position_mismatch_field=self.lag_grid_position_mismatch_field[
+            lag_grid_position_mismatch_field=self.local_lag_grid_position_mismatch_field[
                 ..., : self.local_num_lag_nodes
             ],
-            lag_grid_velocity_mismatch_field=self.lag_grid_velocity_mismatch_field[
+            lag_grid_velocity_mismatch_field=self.local_lag_grid_velocity_mismatch_field[
                 ..., : self.local_num_lag_nodes
             ],
             dt=dt,
